9a9067a0d80de414e31e7049ce69fdbb79156947
sravya372 <39930372+sravya372@users.noreply.github.com>
2018-07-12 19:42:32 -0500

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using icsocial2.Data;
+using icsocial2.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace icsocial2.Pages.Profile
+{
+    [Authorize]
+    public class IndexModel : PageModel
+    {
+
+        private UserManager<ApplicationUser> _userManager;
+
+        private ApplicationDbContext _db;
+        
+        public IndexModel(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IEnumerable<ApplicationUser> Profiles { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            System.Diagnostics.Debug.WriteLine("Getting user id");
+            var userID = _userManager.GetUserId(HttpContext.User);
+            System.Diagnostics.Debug.WriteLine(userID);
+            Profiles = await _db.Users.Where(l => l.Id == userID).ToListAsync();
+
+            System.Diagnostics.Debug.WriteLine($"Number of items: {Profiles.Count()}");
+        }
+
+
+
+    }
+}
